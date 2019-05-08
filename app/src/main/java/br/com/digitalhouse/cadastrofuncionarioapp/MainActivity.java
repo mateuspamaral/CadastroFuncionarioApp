b80e09ba848f35d9253212e544d44d03366eb19e
Mateus Pessoa Amaral <mateuspa@gmail.com>
2019-05-08 15:08:03 -0300

    Initial commit

---
+++ app/src/main/java/br/com/digitalhouse/cadastrofuncionarioapp/MainActivity.java
@@ -0,0 +1,49 @@
+package br.com.digitalhouse.cadastrofuncionarioapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextEmail;
+    EditText editTextPassword;
+    Button buttonEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editTextEmail.getText().toString();
+                String senha = editTextPassword.getText().toString();
+
+                if (email.equals("")) {
+                    Toast.makeText(MainActivity.this, "Email não pode ser vazio", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (senha.length() < 6 || senha.length() > 6) {
+                    Toast.makeText(MainActivity.this, "Senha deve ter 6 caracteres", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (senha.equals("123456")) {
+                    Toast.makeText(MainActivity.this, "Ok pode passar :)", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
